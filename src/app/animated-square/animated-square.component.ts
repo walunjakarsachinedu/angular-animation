aab55624ab397583e4684ff725de4031af830ab5
Sachin Walunjakar <walunjakarsachin@gmail.com>
2024-05-19 14:09:36 +0530

    added void state animation

--- src/app/animated-square/animated-square.component.ts
+++ src/app/animated-square/animated-square.component.ts
@@ -7,11 +7,12 @@ import {
   transition,
   // ...
 } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-animated-square',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './animated-square.component.html',
   styleUrl: './animated-square.component.scss',
   animations: [
@@ -28,13 +29,28 @@ import {
         animate('.3s 0s ease')
       ]),
     ]),
+    trigger("presence", [
+      transition('void => *', [
+        style({ opacity: 0, height: '0px'}),
+        animate('.3s 0s ease', style({opacity: 1, height: '*'}))
+      ]),
+      transition('* => void', [
+        style({ opacity: 1, height: '*'}),
+        animate('.3s 0s ease', style({opacity: 0, height: '0px'}))
+      ])
+    ]),
   ],
 })
 export class AnimatedSquareComponent {
 
   public isOpen=false;
+  public isPresent = false;
 
   toggle() {
     this.isOpen = !this.isOpen;
+  }
+
+  togglePresence() {
+    this.isPresent = !this.isPresent;
   }
 }
