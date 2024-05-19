7650ec91d50260384d8f281e7aeec0dfe6d224d6
Sachin Walunjakar <walunjakarsachin@gmail.com>
2024-05-19 12:26:21 +0530

    added simple trigger animation

--- src/app/animated-square/animated-square.component.ts
+++ src/app/animated-square/animated-square.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-animated-square',
   standalone: true,
   imports: [],
   templateUrl: './animated-square.component.html',
-  styleUrl: './animated-square.component.scss'
+  styleUrl: './animated-square.component.scss',
+  animations: [
+    trigger("openClose", [
+      state('open', style({
+        height: '100px',
+      })),
+      state('closed', style({
+        height: '40px',
+      })),
+      transition('open => closed', [
+        animate('.3s 0s ease')
+      ]),
+      transition('closed => open', [
+        animate('.3s 0s ease')
+      ]),
+    ]),
+  ],
 })
 export class AnimatedSquareComponent {
 
+  public isOpen=false;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
 }
